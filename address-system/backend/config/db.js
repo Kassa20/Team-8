2d769a06a58b555c6bb209a1be640ac71f69dda9
Yohannes <ykassa47@gmail.com>
2026-03-06 10:03:26 -0800

    connecting to MongoDB

--- address-system/backend/config/db.js
+++ address-system/backend/config/db.js
@@ -1,33 +1,39 @@
 const mongoose = require("mongoose");
 
-let memoryServer;
 
 const connectDB = async () => {
-  try {
-    let uri = process.env.MONGO_URI;
 
-    // Dev-friendly fallback so the starter runs without a local Mongo install.
-    // If you set MONGO_URI (local MongoDB or Atlas), that will be used instead.
-    if (!uri) {
-      const { MongoMemoryServer } = require("mongodb-memory-server");
-      memoryServer = await MongoMemoryServer.create();
-      uri = memoryServer.getUri("address_system");
-      // eslint-disable-next-line no-console
-      console.log("Using in-memory MongoDB (mongodb-memory-server)");
-      // TODO: Expand seed script to support large dataset seeding
-    }
+    await mongoose.connect('mongodb://localhost/test')
+        .then(() => console.log('Connected to MongoDB...'))
+        .catch(err => console.error('Could not connect to MongoDB'))
+}
 
-    await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000,
-    });
-    // eslint-disable-next-line no-console
-    console.log("MongoDB connected");
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     let uri = process.env.MONGO_URI;
+
+//     // Dev-friendly fallback so the starter runs without a local Mongo install.
+//     // If you set MONGO_URI (local MongoDB or Atlas), that will be used instead.
+//     if (!uri) {
+//       const { MongoMemoryServer } = require("mongodb-memory-server");
+//       memoryServer = await MongoMemoryServer.create();
+//       uri = memoryServer.getUri("address_system");
+//       // eslint-disable-next-line no-console
+//       console.log("Using in-memory MongoDB (mongodb-memory-server)");
+//       // TODO: Expand seed script to support large dataset seeding
+//     }
+
+//     await mongoose.connect(uri, {
+//       serverSelectionTimeoutMS: 5000,
+//     });
+//     // eslint-disable-next-line no-console
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     // eslint-disable-next-line no-console
+//     console.error("MongoDB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
 
 module.exports = connectDB;
 
