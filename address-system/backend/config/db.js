4ccfebce735a18f34cc9f358881f44701421fd1a
Yohannes <ykassa47@gmail.com>
2026-03-10 15:10:53 -0700

    connected to MongoDB

--- address-system/backend/config/db.js
+++ address-system/backend/config/db.js
@@ -1,39 +1,15 @@
 const mongoose = require("mongoose");
 
-
 const connectDB = async () => {
-
-    await mongoose.connect('mongodb://localhost/test')
-        .then(() => console.log('Connected to MongoDB...'))
-        .catch(err => console.error('Could not connect to MongoDB'))
-}
-
-// const connectDB = async () => {
-//   try {
-//     let uri = process.env.MONGO_URI;
-
-//     // Dev-friendly fallback so the starter runs without a local Mongo install.
-//     // If you set MONGO_URI (local MongoDB or Atlas), that will be used instead.
-//     if (!uri) {
-//       const { MongoMemoryServer } = require("mongodb-memory-server");
-//       memoryServer = await MongoMemoryServer.create();
-//       uri = memoryServer.getUri("address_system");
-//       // eslint-disable-next-line no-console
-//       console.log("Using in-memory MongoDB (mongodb-memory-server)");
-//       // TODO: Expand seed script to support large dataset seeding
-//     }
-
-//     await mongoose.connect(uri, {
-//       serverSelectionTimeoutMS: 5000,
-//     });
-//     // eslint-disable-next-line no-console
-//     console.log("MongoDB connected");
-//   } catch (error) {
-//     // eslint-disable-next-line no-console
-//     console.error("MongoDB connection error:", error.message);
-//     process.exit(1);
-//   }
-// };
+  try {
+    const uri = process.env.MONGO_URI || "mongodb://localhost/test";
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
 
